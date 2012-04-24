cae95200bfc6c700e7820272bbd9082b04663e1d
Günter Weber <webersheim@gmail.com>
2012-04-24 10:07:37 +0200

    changed supplier-mapping to be done via http on daily basis

--- daily/supplier_mapping.php
+++ daily/supplier_mapping.php
@@ -3,18 +3,34 @@
 
 include "../config.inc.php";
 
-$debug = 7;
+$debug = 1;
 // this file adds aliases of manufacturer to a mapping-table
 // TODO: should be run on a daily basis
 
-$file = file_get_contents("/data/icecat/initial/supplier_mapping.xml");
+$context = stream_context_create(array(
+	'http' => array(
+		'header'  => 
+			"Authorization: Basic " . base64_encode("$icecat_user:$icecat_pass") . "\n"
+		)
+));
+
+$file = file_get_contents($daily_supp_url, false, $context);	// load the file over http
+if (!strlen($file)){
+	exit("error on file fetch\n");
+}
 
 $array = json_decode(json_encode(simplexml_load_string($file)), TRUE);
 
 foreach($array[SupplierMappings][SupplierMapping] as $k => $v){
 	$manufacturer_ice_id = (int) pg_escape_string( $v['@attributes'][supplier_id] );
+	$manufacturer_name = pg_escape_string( $v['@attributes'][name] );
 	$manu_check = mydb_query("select * from manufacturer where icecat_id=$manufacturer_ice_id",1);
-	if ($manu_check) {
+	// manufacturer missing
+	if (!$manu_check) {
+		mydb_query("insert into manufacturer (icecat_id, name) values ($manufacturer_ice_id, '$manufacturer_name' )",1);
+		debug(1,"manufacturere $manufacturer_name (#$manufacturer_ice_id) inserted\n");
+		$manu_check = mydb_query("select * from manufacturer where icecat_id=$manufacturer_ice_id",1);
+	}else {
 		$manu_id = (int) $manu_check[id];
 		
 		if ($v['@attributes']){
@@ -27,7 +43,7 @@ foreach($array[SupplierMappings][SupplierMapping] as $k => $v){
 				if (!$map_check){
 					$insert_query = mydb_query("insert into mapping(type,value,mapped_id) values ('manufacturer', '$mapping', $manu_id)",1);
 				} else {
-					echo "mapping found\n";
+					debug(1,"mapping found\n");
 				}
 			}
 		}
